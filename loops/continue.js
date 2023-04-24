79c76026c1c440ced29c0f73ed5b59700519be64
sundararajan-git <sundararajanselvarasu@gmail.com>
2023-04-24 22:03:06 +0530

    loops

---
+++ loops/continue.js
@@ -0,0 +1,21 @@
+console.clear();
+
+// ------------------------ continue--------------------------
+
+let i=0;
+while(i<10){
+    i++
+    if(i==5){
+        continue;
+    }
+    console.log(i);
+}
+
+// --------------------- for loop---------------------
+
+for(let j = 0 ;j<=10;j++){
+    if(j==5){
+        continue;
+    }
+    console.log(j);
+}
---
+++ loops/do while loop.js
@@ -0,0 +1,29 @@
+console.clear();
+
+// ---------------------------do while loop----------------------------
+
+let i = 0;
+do{
+    console.log(i);
+    i++;
+}while(i<=10)
+
+// ---------------------------- odd number---------------------------
+
+let count=0;
+do{
+    if (count%2!=0) {
+        console.log(count);
+    }
+    count++;
+}while(count<=10);
+
+// --------------------------- even number ---------------------------
+
+let x=0;
+do{
+    if (x%2==0) {
+        console.log(x);
+    }
+    x++;
+}while(x<=10);
