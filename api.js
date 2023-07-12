2aa75029f1942e4f5aefd296e304040923be9b16
sundararajan-git <121149600+sundararajan-git@users.noreply.github.com>
2023-07-12 21:38:46 +0530

    Update api.js

--- api.js
+++ api.js
@@ -2,6 +2,6 @@ console.clear();
 // -------------------- API - Application programing interface-----------
 // ---------- To comunicating one program to anothor program
 
-fetch('https://api-thirukkural.vercel.app/api?num=x')
+fetch("https://jsonplaceholder.typicode.com/todos/1")
 .then((res)=>res.json())
-.then((msg)=>console.log(msg[0].setup))
+.then((msg)=>console.log(msg))
